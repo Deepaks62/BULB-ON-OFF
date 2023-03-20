99a52ade691fd9ab3524278c2164c0ee4e8378ef
unknown <deepaksarahyt@gmail.com>
2023-03-20 15:49:35 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+// Turn on funcation
+
+function bulbon(){
+    let bulbobj =document.querySelector(".bulboff");
+    bulbobj.setAttribute("src", "./on.png");
+    
+}
+
+function bulboff(){
+    let bulbobj =document.querySelector(".bulboff");
+    bulbobj.setAttribute("src", "./off .png");
+}
